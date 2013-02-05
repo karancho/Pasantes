c8a0f7520c1c70faf472cdf9e1969c8b235fcaca
Sergio Alonso <sergio@eim.esc.edu.ar>
2013-02-05 16:32:08 -0300

    Gemfile con librerias y dependencias listas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pasantes::Application.config.secret_token = 'e5acf96e727b8858e389338d891e021c9f5e670de4b214e0d423322240f33da715d6bbce23f40e3f14ed2df248d2a6f36d6ced1f837024bf7247aeac636dd99f'
