d145a2150ac77021bbc9b9bb31b1c5d10365699b
Sergio Alonso <escuelaint@gmail.com>
2013-02-07 19:16:50 -0300

    Nivel para administradores funcionando

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :dni, :locality_id, :name, :password_digest, :surname, :email, :password, :password_confirmation
+  attr_accessible :dni, :locality_id, :name, :password_digest, :surname, :email, :password, :password_confirmation, :admin
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
