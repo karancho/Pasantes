f3ef9cf4c8492da70071e49472564e7c881001dc
Sergio Alonso <escuelaint@gmail.com>
2013-02-07 19:02:16 -0300

    proto sistema de logueo de administradores funcionando

--- app/controllers/provinces_controller.rb
+++ app/controllers/provinces_controller.rb
@@ -1,5 +1,8 @@
 class ProvincesController < ApplicationController
-  skip_before_filter :authorize, only: [:create, :update, :destroy]
+  
+  #before_filter :soloadmin
+  
+  #skip_before_filter :authorize, only: [:create, :update, :destroy]
   #skip_before_filter :authorize
   
   # GET /provinces
