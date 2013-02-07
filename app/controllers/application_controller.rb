bd803671a91098977111116f66a4c04bc0ffcb33
Sergio Alonso <escuelaint@gmail.com>
2013-02-07 19:17:57 -0300

    Limpieza de comentarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,6 @@ class ApplicationController < ActionController::Base
       unless User.find_by_id(session[:user_id])
         redirect_to login_url, notice: "Por favor logueese"
       end
-    end
-    
-    #def soloadmin
-    #  user = User.find_by_id(session[:user_id])
-    #  if user.dni == "111"
-    #    puts "todo bien"
-    #  else
-    #    redirect_to login_url, notice: "Necesita nivel de administrador"
-    #  end
-      
-    #end
+    end   
+
 end
