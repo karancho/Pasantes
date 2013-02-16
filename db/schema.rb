34becf56800b27840d38a7cb172cfa6c6d516bc6
Sergio Alonso <sergio@eim.esc.edu.ar>
2013-02-15 21:10:11 -0300

    Agregado cuil a Users

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130209005839) do
+ActiveRecord::Schema.define(:version => 20130216000526) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -81,6 +80,7 @@ ActiveRecord::Schema.define(:version => 20130209005839) do
     t.datetime "updated_at",      :null => false
     t.string   "email"
     t.boolean  "admin"
+    t.string   "cuil"
   end
 
 end
